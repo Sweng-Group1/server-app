a559137d781365b95bf9fdd57416bb9b23b880f0
Sidharth S <sidharth.s16@yahoo.com>
2023-01-23 19:27:33 +0000

    Initial Spring Security configuration
    Override Spring Security so that API endpoints can be accessed without requiring a username and password or a User model.

--- src/main/java/com/sweng22g1/serverapp/ServerAppApplication.java
+++ src/main/java/com/sweng22g1/serverapp/ServerAppApplication.java
@@ -2,12 +2,19 @@ package com.sweng22g1.serverapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ServerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerAppApplication.class, args);
+	}
+	
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }

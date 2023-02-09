128b60720953ab5a08debb38dd528f71eda8a881
Sidharth S <sidharth.s16@yahoo.com>
2023-02-09 13:40:42 +0000

    Created Map entity

---
+++ src/main/java/com/sweng22g1/serverapp/model/Map.java
@@ -0,0 +1,37 @@
+package com.sweng22g1.serverapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Map {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Map name cannot be null")
+	@Size(max = 100, min = 1, message = "Map name length invalid.")
+	@Column(unique = true)
+	private String name;
+	
+	@NotNull(message = "Map filepath cannot be null")
+	@Size(max = 255, min = 1, message = "Map filepath length invalid")
+	@Column(unique = true)
+	private String filepath;
+	
+}

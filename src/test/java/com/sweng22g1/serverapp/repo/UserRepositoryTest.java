1b4fa65981ec7797d5584f5629757884b0b92a8d
Paul Pickering <paulpickering678@gmail.com>
2023-02-19 11:35:47 +0000

    Added Repository test for Role & Post mappings

--- src/test/java/com/sweng22g1/serverapp/repo/UserRepositoryTest.java
+++ src/test/java/com/sweng22g1/serverapp/repo/UserRepositoryTest.java
@@ -3,6 +3,13 @@ package com.sweng22g1.serverapp.repo;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.validation.ConstraintViolationException;
 
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
@@ -13,6 +20,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.sweng22g1.serverapp.model.Post;
+import com.sweng22g1.serverapp.model.Role;
 import com.sweng22g1.serverapp.model.User;
 
 @ActiveProfiles("test")
@@ -55,8 +64,9 @@ public class UserRepositoryTest {
 	}
 	
 	
-	// We expect a ConstraintViolation exception when attempting to fields
-	// with a 'no null value allowed' constraint applied, as per Hibernate documentation. 
+	// We expect a ConstraintViolation exception when attempting to save fields
+	// with a 'no null value allowed' constraint applied, if no value is specified, 
+	// as per Hibernate documentation. 
 	@Test
 	void nullUsernameIsNotAllowed() {
 		//given 
@@ -244,6 +254,46 @@ public class UserRepositoryTest {
 		})
 		.isInstanceOf(ConstraintViolationException.class)
 		.hasMessageContaining("Password length too small");
+	}
+	
+	@Test
+	void retrieveMappedValuesSuccessfully() {
+		//given 
+		
+		LocalDateTime timestampCreated = LocalDateTime.of(2020, Month.JANUARY, 1,1, 1);
+		LocalDateTime timestampUpdated = LocalDateTime.of(2020,Month.FEBRUARY, 1, 1, 1);
+		LocalDateTime timestampExpiry = LocalDateTime.of(2024,Month.JANUARY, 1, 1, 1);
+		double latitude = 50;
+		double longitude = 45;
+		
+		Role newRole = new Role(1L, "testRole");
+		Post newPost = new Post
+				(1L, "XMLContent", timestampCreated, timestampUpdated, timestampExpiry, latitude, longitude);
+		
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(newRole);
+		
+		Set<Post> posts = new HashSet<Post>();
+		posts.add(newPost);
+		
+		User user = User.builder()
+				.username("username")
+				.firstname("firstName")
+				.lastname("lastName")
+				.password("password")
+				.email("user@email.com")
+				.posts(posts)
+				.roles(roles)
+				.build();
+		
+		underTest.save(user);
+		
+		// when
+		User foundUser = underTest.findByUsername("username");
+		
+		// then
+		assertThat(foundUser.getPosts()).isEqualTo(posts);
+		assertThat(foundUser.getRoles()).isEqualTo(roles);
 	}
 	
 	

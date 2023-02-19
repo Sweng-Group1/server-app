7fb11c48fdd4d27efb1cea4e66d28867d6a67795
Paul Pickering <paulpickering678@gmail.com>
2023-02-19 12:45:31 +0000

    Added Test for Duplicate Username

--- src/test/java/com/sweng22g1/serverapp/repo/UserRepositoryTest.java
+++ src/test/java/com/sweng22g1/serverapp/repo/UserRepositoryTest.java
@@ -296,6 +296,72 @@ public class UserRepositoryTest {
 		assertThat(foundUser.getRoles()).isEqualTo(roles);
 	}
 	
+	@Test
+	void AddingUserWithIdenticalUsernameShouldNotBeAllowed() {
+		//given 
+		
+		LocalDateTime timestampCreated = LocalDateTime.of(2020, Month.JANUARY, 1,1, 1);
+		LocalDateTime timestampUpdated = LocalDateTime.of(2020,Month.FEBRUARY, 1, 1, 1);
+		LocalDateTime timestampExpiry = LocalDateTime.of(2024,Month.JANUARY, 1, 1, 1);
+		double latitude = 50;
+		double longitude = 45;
+		
+		Post newPost = Post.builder()
+				.xmlContent("Hello World")
+				.created(timestampCreated)
+				.updated(timestampUpdated)
+				.expiry(timestampExpiry)
+				.latitude(latitude)
+				.longitude(longitude)
+				.build();
+		
+		Role newRole = Role.builder()
+				.name("testRole")
+				.build();
+		
+		
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(newRole);
+		
+		Set<Post> posts = new HashSet<Post>();
+		posts.add(newPost);
+		
+		User user = User.builder()
+				.username("username")
+				.firstname("firstName")
+				.lastname("lastName")
+				.password("password")
+				.email("user@email.com")
+				.posts(posts)
+				.roles(roles)
+				.build();
+		
+		User user2 = User.builder()
+				.username("username")
+				.firstname("John")
+				.lastname("Doe")
+				.password("password2")
+				.email("user@gmail.com")
+				.posts(posts)
+				.roles(roles)
+				.build();
+		
+		underTest.save(user2);
+		
+		
+		// when
+		//User foundUser = underTest.findByUsername("username");
+		//foundUser.setLastname("Smith");
+		//roles.add(secondRole);
+		//foundUser.setRoles(roles);
+		//underTest.save(foundUser);
+		//User foundUser2 = underTest.findByUsername("username");
+		
+		// then
+		//assertThat(foundUser.getPosts()).isEqualTo(posts);
+		//assertThat(foundUser.getRoles()).isEqualTo(roles);
+	}
+	
 	
 	
 	

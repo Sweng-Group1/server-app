148bb347aeb790a35edfbb1f36e6dbff8752fc4a
Sidharth S <sidharth.s16@yahoo.com>
2023-03-08 18:27:34 +0000

    Added auth endpoint rules

--- src/main/java/com/sweng22g1/serverapp/security/SecurityConfig.java
+++ src/main/java/com/sweng22g1/serverapp/security/SecurityConfig.java
@@ -44,10 +44,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/api/v1/ping").permitAll();
 		http.authorizeRequests().antMatchers("/api/v1/login/**", "/api/v1/token/refresh/**").permitAll();
 //		TODO: Add auth rules to endpoints once endpoints have been added!
-//		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/user/**").hasAnyAuthority("User");
-//		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/role/**").hasAnyAuthority("Admin", "SuperAdmin");
-//		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/user/**").hasAnyAuthority("Admin", "SuperAdmin");
-//		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/role/**").hasAnyAuthority("Admin");
+
+		// GET Post - anyone can get posts, in the endpoint (controller layer) we need
+		// to define rules to make sure logged out users can only see verified user
+		// posts.
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/post/**").permitAll();
+		// POST Post - all logged in users can upload a post
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/post/**").hasAnyAuthority("User", "Verfied",
+				"Admin");
+
+		// GET Media - anyone can get media
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/media/**").permitAll();
+		// POST Media - all logged in users can upload media
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/media/**").hasAnyAuthority("User", "Verfied",
+				"Admin");
+
+		// POST User - no authentication is needed to create users however if a user is
+		// not verified or an admin they can only modify their own entity.
+		// TODO service layer this rule.
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/user/**").permitAll();
+		// GET User - logged in users can get users.
+		// TODO: Add controller layer logic to make sure any user that's not an admin or
+		// verified can only GET their own User entity.
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/user/**").permitAll();
+
+		// POST Map - only admins or verified users can POST map entities
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/map/**").hasAnyAuthority("Verfied", "Admin");
+		// GET Map - all users can get maps
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/map/**").permitAll();
+
 		http.authorizeRequests().anyRequest().authenticated();
 		http.addFilter(customAuthenticationFilter);
 		http.addFilterBefore(customAuthorisationFilter, UsernamePasswordAuthenticationFilter.class);

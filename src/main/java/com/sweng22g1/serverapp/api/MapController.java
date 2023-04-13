41d85676ec83f938bf4ccd5fe0fefc95ecba46f8
Sidharth S <sidharth.s16@yahoo.com>
2023-04-13 20:12:23 +0100

    Removed filepath print out from uploadMap

--- src/main/java/com/sweng22g1/serverapp/api/MapController.java
+++ src/main/java/com/sweng22g1/serverapp/api/MapController.java
@@ -56,7 +56,7 @@ public class MapController {
 		try {
 			log.info("Attempting to handle map upload, name=" + mapName);
 			Map createdMap = mapService.createMap(mapName, mapFile.getBytes());
-			message = "Map has been saved! Name: " + mapName + " | id: " + String.valueOf(createdMap.getId() + " | filename: " + createdMap.getFilepath());
+			message = "Map has been saved! Name: " + mapName + " | id: " + String.valueOf(createdMap.getId());
 			return ResponseEntity.ok().body(message);
 		} catch (Exception e) {
 			log.error("Map upload endpoint fail, exception=" + e.getMessage());
